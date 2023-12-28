67f0740d5e09d5a62f55d52350f8598628cc32d3
mihirtakalkar <mihirtakalkar@gmail.com>
2023-12-28 15:51:08 -0800

    Generated Expo App and added JS boilerplate code.

---
+++ WeatherExpo/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View style = {styles.container}>
+        <Text>Current Weather</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'pink'
+  }
+})
+export default App;
