030a54b1c5a2d5d196bc85411de38bc1a0d4ce76
mihirtakalkar <mihirtakalkar@gmail.com>
2023-12-28 20:44:11 -0800

    Added static weather display for Upcoming Weather.

--- WeatherExpo/App.js
+++ WeatherExpo/App.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import {View, StyleSheet} from "react-native";
+import CurrentWeather from "./src/components/CurrentWeather";
+import UpcomingWeather from "./src/components/UpcomingWeather";
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <View style = {styles.container}>
-        <Text>Current Weather</Text>
-      </View>
-    </SafeAreaView>
-  );
-};
+  return(
+    <View style = {styles.container}>
+      <UpcomingWeather />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'pink'
+    flex: 1
   }
 })
+
 export default App;
---
+++ WeatherExpo/src/components/CurrentWeather.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import {Feather} from '@expo/vector-icons';
+
+const CurrentWeather = () => {
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Feather name="sun" size={100} color="black"></Feather>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>Its sunny</Text>
+        <Text style={styles.message}>Its prefect t-shirt weather</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: "pink",
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  temp: {
+    color: "black",
+    fontSize: 48,
+  },
+  feels: {
+    color: "black",
+    fontSize: 30,
+  },
+  highlow: {
+    color: "black",
+    fontSize: 20,
+  },
+  highLowWrapper: {
+    flexDirection: "row",
+  },
+  bodyWrapper: {
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+    paddingLeft: 25,
+    marginBottom: 40
+  },
+  description: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
+  }
+});
+export default CurrentWeather;
